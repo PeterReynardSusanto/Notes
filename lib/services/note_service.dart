9bdfdaa22d3c649f56bb75a97c4ebd1084df5082
JonathanF123 <jonathanlevid3@gmail.com>
2024-04-16 11:40:02 +0700

    update notes

---
+++ lib/services/note_service.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoteService extends StatefulWidget {
+  const NoteService({super.key});
+
+  @override
+  State<NoteService> createState() => _NoteServiceState();
+}
+
+class _NoteServiceState extends State<NoteService> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
