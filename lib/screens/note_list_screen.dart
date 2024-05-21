3281ac635a59f6888637cb24cfba872e0203375c
PeterReynardSusanto <Peterreynard20@gmail.com>
2024-05-21 11:22:49 +0700

    add edit_screen

--- lib/screens/note_list_screen.dart
+++ lib/screens/note_list_screen.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:notes/screens/note_edit_screen.dart';
 import 'package:notes/services/note_service.dart';
 import 'package:notes/widgets/note_dialog.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
 class NoteListScreen extends StatefulWidget {
   const NoteListScreen({super.key});
-
 
   @override
   State<NoteListScreen> createState() => _NoteListScreenState();
 }
-
 
 class _NoteListScreenState extends State<NoteListScreen> {
   @override
@@ -23,11 +21,11 @@ class _NoteListScreenState extends State<NoteListScreen> {
       body: const NoteList(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return const NoteDialog();
-            },
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => NoteEditScreen(),
+            ),
           );
         },
         tooltip: 'Add Note',
@@ -37,10 +35,8 @@ class _NoteListScreenState extends State<NoteListScreen> {
   }
 }
 
-
 class NoteList extends StatelessWidget {
   const NoteList({super.key});
-
 
   Future<void> _launchMaps(double latitude, double longitude) async {
     Uri googleUrl = Uri.parse(
@@ -52,7 +48,6 @@ class NoteList extends StatelessWidget {
       // Optionally, show a message to the user
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -74,86 +69,94 @@ class NoteList extends StatelessWidget {
                 return Card(
                   child: InkWell(
                     onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return NoteDialog(note: document);
-                        },
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => NoteEditScreen(note: document),
+                        ),
                       );
                     },
-                    child: Column(
-                      children: [
-                        document.imageUrl != null &&
-                                Uri.parse(document.imageUrl!).isAbsolute
-                            ? ClipRRect(
-                                borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(16),
-                                  topRight: Radius.circular(16),
-                                ),
-                                child: Image.network(
-                                  document.imageUrl!,
-                                  fit: BoxFit.cover,
-                                  alignment: Alignment.center,
-                                  width: double.infinity,
-                                  height: 150,
-                                ),
-                              )
-                            : Container(),
-                        ListTile(
-                          title: Text(document.title),
-                          subtitle: Text(document.description),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(
-                                icon: const Icon(Icons.map),
-                                onPressed: document.latitude != null &&
-                                        document.longitude != null
-                                    ? () {
-                                        _launchMaps(document.latitude!,
-                                            document.longitude!);
-                                      }
-                                    : null, // Disable the button if latitude or longitude is null
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title: const Text('Konfirmasi Hapus'),
-                                        content: Text(
-                                            'Yakin ingin menghapus data \'${document.title}\' ?'),
-                                        actions: <Widget>[
-                                          TextButton(
-                                            child: const Text('Cancel'),
-                                            onPressed: () {
-                                              Navigator.of(context).pop();
-                                            },
-                                          ),
-                                          TextButton(
-                                            child: const Text('Hapus'),
-                                            onPressed: () {
-                                              NoteService.deleteNote(document)
-                                                  .whenComplete(() =>
-                                                      Navigator.of(context)
-                                                          .pop());
-                                            },
-                                          ),
-                                        ],
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            document.imageUrl != null &&
+                                    Uri.parse(document.imageUrl!).isAbsolute
+                                ? ClipRRect(
+                                    borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(16),
+                                      topRight: Radius.circular(16),
+                                    ),
+                                    child: Image.network(
+                                      document.imageUrl!,
+                                      fit: BoxFit.cover,
+                                      alignment: Alignment.center,
+                                      width: double.infinity,
+                                      height: 150,
+                                    ),
+                                  )
+                                : Container(),
+                            ListTile(
+                              title: Text(document.title),
+                              subtitle: Text(document.description),
+                              trailing: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconButton(
+                                    icon: const Icon(Icons.map),
+                                    onPressed: document.latitude != null &&
+                                            document.longitude != null
+                                        ? () {
+                                            _launchMaps(document.latitude!,
+                                                document.longitude!);
+                                          }
+                                        : null, // Disable the button if latitude or longitude is null
+                                  ),
+                                  InkWell(
+                                    onTap: () {
+                                      showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return AlertDialog(
+                                            title:
+                                                const Text('Konfirmasi Hapus'),
+                                            content: Text(
+                                                'Yakin ingin menghapus data \'${document.title}\' ?'),
+                                            actions: <Widget>[
+                                              TextButton(
+                                                child: const Text('Cancel'),
+                                                onPressed: () {
+                                                  Navigator.of(context).pop();
+                                                },
+                                              ),
+                                              TextButton(
+                                                child: const Text('Hapus'),
+                                                onPressed: () {
+                                                  NoteService.deleteNote(
+                                                          document)
+                                                      .whenComplete(() =>
+                                                          Navigator.of(context)
+                                                              .pop());
+                                                },
+                                              ),
+                                            ],
+                                          );
+                                        },
                                       );
                                     },
-                                  );
-                                },
-                                child: const Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 10),
-                                  child: Icon(Icons.delete),
-                                ),
+                                    child: const Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 10),
+                                      child: Icon(Icons.delete),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
                 );
